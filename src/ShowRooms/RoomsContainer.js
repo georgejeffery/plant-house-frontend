e95875b4e96aa81eb5664fb7cfb98972fa38947f
seenevz <serneves@hotmail.com>
2019-03-06 13:51:30 +0000

    update 5

--- src/ShowRooms/RoomsContainer.js
+++ src/ShowRooms/RoomsContainer.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Segment, Header } from "semantic-ui-react";
+import RoomCard from "./RoomCard";
 
 export default class RoomsContainer extends React.Component {
 
+    state = {
+        RoomClicked: null
+    };
+
     render () {
-        // const rooms = this.props.rooms.map()
+
         return(
-            this.props.rooms
+            <>
+            {this.props.rooms
                ? <Segment>
-                   {}
+                   {this.props.rooms.map(room => <RoomCard key={room.name} room={room}/>)}
                  </Segment> 
                : <Segment placeholder>
-                    <Header icon>It looks like you don't have any  yet!</Header>
-                </Segment>
+                    <Header icon>It looks like you don't have any... yet!</Header>
+                </Segment>}
+            </>
 
         );
     };
