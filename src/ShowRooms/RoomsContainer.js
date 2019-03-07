a03195d903ffa199e9c1ec6609368d8188d10c6b
seenevz <serneves@hotmail.com>
2019-03-07 09:31:57 +0000

    update 5

--- src/ShowRooms/RoomsContainer.js
+++ src/ShowRooms/RoomsContainer.js
@@ -2,27 +2,37 @@ import React from "react";
 import { Segment, Header } from "semantic-ui-react";
 import RoomCard from "./RoomCard";
 import PlantsContainer from "../ShowPlants/PlantsContainer";
+import API from "../API";
 
 export default class RoomsContainer extends React.Component {
 
     state = {
-        roomClicked: null
+        roomClicked: null,
+        rooms:this.props.user.rooms,
+        plants:[],
     };
 
     handleRoomSelect = (roomClicked) => {
-        this.setState({ roomClicked });
+        this.setState({ roomClicked }, () => this.updatePlants());
+
     }; 
+
+    updatePlants = () => {
+            API.getRoom(this.state.roomClicked.id)
+                .then(room => this.setState({plants: room.plants}))
+    };
 
     render () {
 
         const roomRender = () => {
             if (this.state.roomClicked) {
-                return <PlantsContainer plants={this.state.roomClicked.plants}/>
+                // debugger
+                return <PlantsContainer plants={this.state.plants}/>
             }
             else {
-                return (this.props.rooms.length
+                return (this.state.rooms.length
                     ? <Segment.Group horizontal style={{overflow:'auto', whiteSpace:'nowrap', minHeight:'80vh'}}>
-                        {this.props.rooms.map(room => <RoomCard handleRoomSelect={this.handleRoomSelect} key={room.name} room={room}/>)}
+                        {this.state.rooms.map(room => <RoomCard handleRoomSelect={this.handleRoomSelect} key={room.name} room={room}/>)}
                       </Segment.Group> 
                     : <Segment placeholder style={{minHeight: '80vh'}}>
                          <Header icon>It looks like you don't have any... yet!</Header>
