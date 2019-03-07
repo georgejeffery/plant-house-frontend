4d79adafffa5285dd87f9e9139c341b25b602fa7
seenevz <serneves@hotmail.com>
2019-03-07 11:40:46 +0000

    fixed buttons behaviour

--- src/ShowRooms/RoomsContainer.js
+++ src/ShowRooms/RoomsContainer.js
@@ -5,34 +5,18 @@ import PlantsContainer from "../ShowPlants/PlantsContainer";
 import API from "../API";
 
 export default class RoomsContainer extends React.Component {
-
-    state = {
-        roomClicked: null,
-        rooms:this.props.user.rooms,
-        plants:[],
-    };
-
-    handleRoomSelect = (roomClicked) => {
-        this.setState({ roomClicked }, () => this.updatePlants());
-
-    }; 
-
-    updatePlants = () => {
-            API.getRoom(this.state.roomClicked.id)
-                .then(room => this.setState({plants: room.plants}))
-    };
-
+ 
     render () {
 
         const roomRender = () => {
-            if (this.state.roomClicked) {
+            if (this.props.roomClicked) {
                 // debugger
-                return <PlantsContainer plants={this.state.plants}/>
+                return <PlantsContainer plants={this.props.plants}/>
             }
             else {
-                return (this.state.rooms.length
+                return (this.props.rooms.length
                     ? <Segment.Group horizontal style={{overflow:'auto', whiteSpace:'nowrap', minHeight:'80vh'}}>
-                        {this.state.rooms.map(room => <RoomCard handleRoomSelect={this.handleRoomSelect} key={room.name} room={room}/>)}
+                        {this.props.rooms.map(room => <RoomCard handleRoomSelect={this.props.handleRoomSelect} key={room.name} room={room}/>)}
                       </Segment.Group> 
                     : <Segment placeholder style={{minHeight: '80vh'}}>
                          <Header icon>It looks like you don't have any... yet!</Header>
