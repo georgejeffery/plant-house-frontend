12a9437eedc0451d01eb9524cc93678405b01af4
georgejeffery <georgejeffery@gmail.com>
2019-03-07 11:05:13 +0000

    STYLING!

--- src/ShowPlants/PlantCard.js
+++ src/ShowPlants/PlantCard.js
@@ -5,24 +5,33 @@ import leafIcon from "./leaf_2.svg";
 import PlantCardBack from "./PlantCardBack";
 import PlantCardFront from "./PlantCardFront";
 
-
 export default class PlantCard extends React.Component {
+  state = {
+    isClicked: false
+  };
 
-    state = {
-        isClicked: false,
-    }
+  handleClickImage = () => {
+    this.setState({ isClicked: !this.state.isClicked });
+  };
 
-    handleClickImage = () => {
-        this.setState({isClicked: !this.state.isClicked})
-    };
+  render() {
+    const plant = this.props.plant;
 
-    render() {
-        const plant = this.props.plant
-
-        return (
-            this.state.isClicked
-                ? <PlantCardBack header={plant.commonname}   handleClick={this.handleClickImage} />
-                : <PlantCardFront image={plant.image_url} {...this.props} meta={plant.plant_habit} header={plant.commonname} onClickSelect ={this.props.onClickSelect} handleClick={this.handleClickImage}/>
-        );
-    }; 
-};
+    return this.state.isClicked ? (
+      <PlantCardBack
+        header={plant.commonname}
+        handleClick={this.handleClickImage}
+      />
+    ) : (
+      <PlantCardFront
+        image={plant.image_url}
+        {...this.props}
+        meta={plant.plant_habit}
+        header={plant.commonname ? plant.commonname : plant.latin_name}
+        onClickSelect={this.props.onClickSelect}
+        handleClick={this.handleClickImage}
+        style={{ width: 250 }}
+      />
+    );
+  }
+}
